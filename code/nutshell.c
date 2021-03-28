ed51fd871024dee147b7075737d54f3b7317dcb6
amills12 <alexandermills@ufl.edu>
2021-03-28 04:29:44 +0000

    Formatting

--- code/nutshell.c
+++ code/nutshell.c
@@ -1,21 +1,20 @@
-#include <stdio.h>    
+#include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include "nutshparser.tab.h"
 
-//1 for auto testing 0 for manual
-#define AUTO 1
+#define AUTO 1 //1 for auto testing, 0 for manual.
 
-typedef struct yy_buffer_state * YY_BUFFER_STATE;
+typedef struct yy_buffer_state *YY_BUFFER_STATE;
 extern int yyparse();
-extern YY_BUFFER_STATE yy_scan_string(char * str);
+extern YY_BUFFER_STATE yy_scan_string(char *str);
 extern void yy_delete_buffer(YY_BUFFER_STATE buffer);
 extern char **environ;
 
 void yyerror(char *s)
 {
     fprintf(stderr, "An Error Has Occured: %s", s);
-}    
+}
 
 int yywrap(void)
 {
@@ -25,7 +24,7 @@ int yywrap(void)
 void printenv()
 {
     int id = 0;
-    while(environ[id] != NULL)
+    while (environ[id] != NULL)
     {
         printf("%s\n", environ[id++]);
     }
@@ -36,7 +35,7 @@ int main()
     printf("Welcome to the NUTSHELL\n");
     printf("%s ", "%");
 
-#if AUTO //If auto is 1 run this code
+#if AUTO //If AUTO is 1 this code will run
     char* testArr[] = { "Yeet", "alias beetle \"beetle juice\"", "bye" };
                     //    "\"nutshell/nutshell/nutshell/nutshell\"" /*This should print quote word quote*/,
                     //    "setenv beetle juice", "printenv beetle", "unsentenv beetle", "printenv beetle",
@@ -45,13 +44,13 @@ int main()
                     //    "bye", "Bye"};
 
     for (int i = 0; i < sizeof(testArr); i++)
-    {	
-	    YY_BUFFER_STATE buffer = yy_scan_string(testArr[i]);
+    {
+        YY_BUFFER_STATE buffer = yy_scan_string(testArr[i]);
         yyparse();
         yy_delete_buffer(buffer);
     }
     return 0;
-#else
+#else //If AUTO is 0 this code will run
     return yyparse();
 #endif
 }
